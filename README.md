1e191aefbf4a7d3ea6c44b16b1333c954201364c
code-examples <no_mames@mail.ru>
2014-12-24 23:05:11 +0000

    02. Creating a Walking Skeleton : 06. Adding Stylus

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-MEANAppsFiles
+Building-AngularJS-and-Node.js-Apps-with-the-MEAN-Stack  
 =============
-This course is up to date. 
-
-Notes:
-To run the node server, type "node web-server.js" or "nodemon web-server.js" from the directory that contains the web-server.js file
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var express = require('express');
+var express = require('express'),
+    stylus  = require('stylus');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
 app.configure(function(){
     app.set('views', __dirname + '/server/views');
     app.set('view engine', 'jade');
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(stylus.middleware(
+        {
+            src: __dirname + '/public',
+            compile: compile
+        }
+    ));
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get('*', function(req, res){
