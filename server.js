bc6ab432b2c5f1056294d90ac565a8c029cbca95
code-examples <no_mames@mail.ru>
2014-12-25 10:05:07 +0000

    02. Creating a Walking Skeleton : 10. Displaying Data in a View

--- server.js
+++ server.js
@@ -25,19 +25,36 @@ app.configure(function(){
 });
 
 mongoose.connect('mongodb://localhost/multivision');
+
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection errror...'));
+db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback(){
     console.log('multivision db opened');
 });
 
+
+var messageSchema = mongoose.Schema({ message: String });
+
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+
+
+// Message.findOne().exec(function(err, messageDoc){
+//     mongoMessage = messageDoc.message;
+// });
+
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
 
 app.get('/partials/:partialPath', function(req, res){
     res.render('partials/' + req.params.partialPath)
 });
 
 app.get('*', function(req, res){
-    res.render('index')
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
 var port = 8080;
