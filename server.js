44f7f612ed134b2dcf65f70d7376066f2a104143
code-examples <no_mames@mail.ru>
2014-12-25 04:22:56 +0000

    02. Creating a Walking Skeleton : 08. Creating the Angular Application

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials/' + req.params.partialPath)
+});
+
 app.get('*', function(req, res){
     res.render('index')
 });
