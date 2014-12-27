c28dd60058975a68b9946d6a4bc00315528e7668
code-examples <no_mames@mail.ru>
2014-12-27 05:27:44 +0000

    05. Authentication : Creating the Client Login Code | 08. Improving the Server Login Code

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,23 +1,11 @@
-var passport = require('passport');
+var auth = require('./auth');
 
 module.exports = function(app){
     app.get('/partials/*', function(req, res){
         res.render('../../public/app/' + req.params)
     });
 
-    app.post('/login', function(req, res, next){
-        var auth = passport.authenticate('local', function(err, user){
-
-            if(err) {return next(err);}
-            if(!user){res.send({success:false})}
-
-            req.login(user,function(err){
-                if(err){return next(err);}
-                res.send({success:true, user: user});
-            })
-        })
-        auth(req, res, next);
-    });
+    app.post('/login', auth.authenticate);
 
     app.get('*', function(req, res){
         res.render('index');
