01032062af48166597371c33783bbd10cbcc39a2
code-examples <no_mames@mail.ru>
2014-12-27 01:35:49 +0000

    05. Authentication : 04. Preparing for Login

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -10,4 +10,20 @@ module.exports = function(config){
         console.log('multivision db opened');
     });
 
+    var userSchema = mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        username: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection){
+        if(collection.length === 0){
+            User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe'});
+            User.create({firstName: 'John', lastName: 'Papa', username: 'john'});
+            User.create({firstName: 'Dan', lastName: 'Wahlin', username: 'dan'});
+        }
+    })
+
 }
