0f48760ef21b293b960b74cbd597f6cd4ddf194f
code-examples <no_mames@mail.ru>
2014-12-28 03:55:36 +0000

    06. Security and Authorization | 05. Refactoring Server Authentication Code

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-var express  = require('express'),
-    mongoose = require('mongoose'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+var express  = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,36 +9,7 @@ var config = require('./server/config/config')[env];
 require('./server/config/express')(app, config);
 require('./server/config/mongoose')(config);
 
-var User = mongoose.model('User');
-
-passport.use(new LocalStrategy(
-    function(username, password, done){
-        User.findOne({ username:username }).exec(
-            function(err, user){
-                if(user && user.authenticate(password)){
-                    return done(null, user);
-                } else {
-                    return done(null, false);
-                }
-            })
-    }
-));
-
-passport.serializeUser(function(user, done){
-    if(user){
-        done(null, user._id);
-    }
-});
-
-passport.deserializeUser(function(id, done){
-    User.findOne({_id:id}).exec(function(err, user){
-        if(user){
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    })
-});
+require('./server/config/passport')();
 
 require('./server/config/routes')(app);
 
