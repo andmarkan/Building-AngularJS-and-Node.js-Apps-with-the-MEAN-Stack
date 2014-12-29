4d4883dd6a3e683402fead3a7e9d69c7dbc96ec7
code-examples <no_mames@mail.ru>
2014-12-29 12:32:01 +0000

    11. Course Details - Selecting and Caching Single Entities | 04. Getting Cached Data

--- public/app/courses/mvCourseDetailCtrl.js
+++ public/app/courses/mvCourseDetailCtrl.js
@@ -1,3 +1,9 @@
-angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCourse, $routeParams){
-    $scope.course = mvCourse.get({_id:$routeParams.id})
-})
+angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCachedCourses, $routeParams){
+    mvCachedCourses.query().$promise.then(function(collection){
+        collection.forEach(function(course){
+            if(course._id === $routeParams.id){
+                $scope.course = course;
+            }
+        })
+    })
+});
