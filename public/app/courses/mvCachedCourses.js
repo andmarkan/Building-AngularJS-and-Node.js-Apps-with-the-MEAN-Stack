59bc3b7af786a69ad0ff3def424b24086bcced54
code-examples <no_mames@mail.ru>
2014-12-29 11:17:22 +0000

    10. Course List - Sorting, Filtering and Caching | 06. Caching Data

---
+++ public/app/courses/mvCachedCourses.js
@@ -0,0 +1,12 @@
+angular.module('app').factory('mvCachedCourses', function(mvCourse){
+    var courseList;
+
+    return {
+        query: function(){
+            if(!courseList){
+                courseList = mvCourse.query();
+            }
+            return courseList;
+        }
+    }
+})
